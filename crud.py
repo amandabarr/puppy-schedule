2a7765d86f1492a696cd4bb35830412f6dee1869
Amanda Barr <amanda.j.webster86@gmail.com>
2021-02-16 10:03:01 -0800

    Added csv data file of past events to the database

--- crud.py
+++ crud.py
@@ -35,12 +35,21 @@ def import_old_events():
 
         # call add event with that info
 
-    pass
+    data_file = open("past-data.csv", "r")
+    data_list = data_file.readlines()
+    for line in data_list:
+        data_list = line.split(",")
+        # for data in data_list:
+        event_type = data_list[0]
+        event_time = str(int(datetime.strptime(
+            data_list[1], '%Y-%m-%d %H:%M').timestamp()))
+        event_comment = data_list[2][1:-2]
+        event_location = data_list[3].strip()
+        add_event(event_type, event_time, event_location, event_comment)
 
 
 if __name__ == '__main__':
     from server import app
     from model import connect_to_db
     connect_to_db(app)
-    add_event("poop")
-    print(get_all_events())
+    import_old_events()
