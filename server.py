f1fcefaed927982c02ab7a83a1805652740064d5
Amanda Barr <amanda.j.webster86@gmail.com>
2021-02-16 23:50:26 -0800

    Set port for flask to run on when using Heroku

--- server.py
+++ server.py
@@ -47,4 +47,5 @@ def add_event():
 if __name__ == '__main__':
     load_dotenv()
     connect_to_db(app)
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
